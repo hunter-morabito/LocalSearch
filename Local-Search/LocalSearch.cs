687f00661ce2b660e34e2dd921d5eaf50722b1da
Hunter Morabito <hunter.morabito@gmail.com>
2017-09-21 22:04:18 -0400

    Project Created

---
+++ Local-Search/LocalSearch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Local_Search
+{
+    class LocalSearch
+    {
+        public static void Main()
+        {
+            Console.Write("hello");
+        }
+    }
+}
